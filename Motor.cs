0ee02a11b798f9defa92f6dddd1cb84c1c5dbfb2
vibhoraz400 <vibhoraz400@outlook.com>
2024-10-01 15:43:36 +0530

    Add project files.

---
+++ Motor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VBNamespace;
+namespace EnumDemo
+{
+     class Motor
+    {
+        public void TestMotor()
+        {
+            int motorOp = (int)MotorFunctions.Stop;
+            Program pro = new Program();
+            pro.MotorFunction(motorOp);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+using EnumDemo;
+namespace VBNamespace
+{
+    public enum MotorFunctions
+    {
+        Start = 1,
+        Stop,
+        Reset
+    }
+    public enum StudentResult
+    {
+        Pass = 1,
+        Fail
+    }
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            // FileStream fs=new FileStream("",FileMode.Create,FileAccess.Write);
+            Motor motor = new Motor();
+            motor.TestMotor();
+        }
+        public void MotorFunction(int motorOperation)
+        {
+            if (motorOperation == (int)MotorFunctions.Start)
+            {
+                Console.WriteLine("Motor has started");
+            }
+            if (motorOperation == (int)MotorFunctions.Stop)
+            {
+                Console.WriteLine("Motor has stopped");
+            }
+            if (motorOperation == (int)MotorFunctions.Reset)
+            {
+                Console.WriteLine("Motor has reset");
+            }
+        }
+    }
+}
